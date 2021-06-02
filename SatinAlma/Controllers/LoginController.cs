850133c1dcc5600cd421d3c0df50fc2b4c0d173c
omerkarikutal <karikutalomer@gmail.com>
2021-06-02 23:35:37 +0300

    Add project files.

---
+++ SatinAlma/Controllers/LoginController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SatinAlma.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        //Action
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(string username, string password)
+        {
+            using (var context = new OBA_ELEKTRAEntities())
+            {
+                var user = context.USER.Where(s => s.KODU == username && s.SIFRESI == password).FirstOrDefault();
+
+                if (user != null)
+                {
+                    Session["user"] = user;
+                    return RedirectToAction("Index", "Home");
+                }
+                return Json(null);
+            }
+        }
+    }
+}
